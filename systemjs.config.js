1a8bea84da313933d2f60a339ef59a9482c67f4c
davinkevin <davin.kevin@gmail.com>
2016-05-10 13:39:01 +0200

    chore(build): in dev-mode, use a distinct folder to put the *.js files

--- systemjs.config.js
+++ systemjs.config.js
@@ -5,7 +5,7 @@
 
     // map tells the System loader where to look for things
     var map = {
-        'app':                        'app', // 'dist',
+        'app':                        'dist', // 'dist',
         'rxjs':                       'node_modules/rxjs',
         'angular2-in-memory-web-api': 'node_modules/angular2-in-memory-web-api',
         '@angular':                   'node_modules/@angular'
@@ -45,5 +45,5 @@
     if (global.filterSystemConfig) { global.filterSystemConfig(config); }
 
     System.config(config);
-    
+
 })(this);
